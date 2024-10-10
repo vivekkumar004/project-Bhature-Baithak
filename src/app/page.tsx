f7a93bf493e9550327e71e582476a4a77346b78b
vivek kumar <vivekk9039@gmail.com>
2024-10-11 00:34:51 +0530

    configuration changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import CssBaseline from "@mui/material/CssBaseline";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <header>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <title>Some name</title>
-      </header>
-      <main className={styles.main}>
-      <CssBaseline />
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+      <header></header>
+      <main>
+        <h1 style={{fontFamily:"Poppins"}}>hello</h1>
+        <h1>hello</h1>
       </main>
-      <footer className={styles.footer}>
-
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
